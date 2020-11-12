ac17e5b6bf8461e8693ae15d1307e44da087c44a
david cortesi <david.cortesi.097@gmail.com>
2020-11-12 17:24:53 +0100

    commit 5
    milestone 1

--- js/main.js
+++ js/main.js
@@ -3,25 +3,103 @@
 // Popolare l'interfaccia, come visto all'assegnazione,  con i dati del vostro utente (Nome utente) e i dati dei contatti (Michele, Fabio, Samuele, Luisa) utilizzando i data di vue.
 // Sempre in allegato troverete le imagini per sfondo e vari avatar.
 // Per le icone utilizzate pure fontawesome.
-const container = new Vue({
-  el:".container",
-  data: {
-    pictures: [
-      {
-        name: "io", img: "_io"
-      },
-      {
-        name: "michele", img: "_1"
-      },
-      {
-        name: "fabio", img: "_2"
-      },
-      {
-        name: "samuele", img: "_3"
-      },
-      {
-        name: "luisa", img: "_4"
-      },
-    ],
-  }
+/**
+ * Boolzapp Vue
+ */
+var app = new Vue({
+    el: '.app',
+    data: {
+        // nostro account
+        user: {
+            name: 'Nome Utente',
+            avatar: '_io'
+        },
+        // Elenco contatti
+          contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+                visible: true,
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:00',
+                        message: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        message: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+                visible: true,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        message: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        message: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        message: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+        ]
+    },
+    methods: {}
 });
