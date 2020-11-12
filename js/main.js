b914450e56612bf74c9417cf6a10e450c722c52a
david cortesi <david.cortesi.097@gmail.com>
2020-11-12 12:39:50 +0100

    commit 1

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Riprodurre il layout cosi come nella grafica (allegata sotto) utilizzando HTML e CSS. Cercate di essere il più fedeli possibile.
+// Utilizare come file js per l'applicazione il main.js che trovate  in allegato
+// Popolare l'interfaccia, come visto all'assegnazione,  con i dati del vostro utente (Nome utente) e i dati dei contatti (Michele, Fabio, Samuele, Luisa) utilizzando i data di vue.
+// Sempre in allegato troverete le imagini per sfondo e vari avatar.
+// Per le icone utilizzate pure fontawesome.
