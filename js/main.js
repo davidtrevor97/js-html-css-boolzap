5bccc0ddbeb837789a29dd3d732cd61b0cbc71af
david cortesi <david.cortesi.097@gmail.com>
2020-11-12 16:35:26 +0100

    commit 4

--- js/main.js
+++ js/main.js
@@ -3,3 +3,25 @@
 // Popolare l'interfaccia, come visto all'assegnazione,  con i dati del vostro utente (Nome utente) e i dati dei contatti (Michele, Fabio, Samuele, Luisa) utilizzando i data di vue.
 // Sempre in allegato troverete le imagini per sfondo e vari avatar.
 // Per le icone utilizzate pure fontawesome.
+const container = new Vue({
+  el:".container",
+  data: {
+    pictures: [
+      {
+        name: "io", img: "_io"
+      },
+      {
+        name: "michele", img: "_1"
+      },
+      {
+        name: "fabio", img: "_2"
+      },
+      {
+        name: "samuele", img: "_3"
+      },
+      {
+        name: "luisa", img: "_4"
+      },
+    ],
+  }
+});
